f56d7cd25234694fe1a928c8e39d07a0fafbd0f2
taewaem <monkey587@naver.com>
2025-09-24 16:22:54 +0900

    fix: 수료증 등록 에러 해결

--- backend/src/main/java/org/jbd/backend/dashboard/controller/CertificateRequestController.java
+++ backend/src/main/java/org/jbd/backend/dashboard/controller/CertificateRequestController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/certificate-requests")
+@RequestMapping("/certificates")
 @RequiredArgsConstructor
 public class CertificateRequestController {
 
